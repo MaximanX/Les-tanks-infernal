c653b1b1bf629dc220731e195a8a4d769b842120
NathanGagnon <nathan.gagnon11@gmail.com>
2017-02-15 15:58:01 -0500

    Add files via upload

---
+++ Projet_Finale/Les-Tanks-Infernal/src/projectiles/ProjectilesFactory.java
@@ -0,0 +1,22 @@
+package projectiles;
+
+import java.util.ArrayList;
+
+import tanks.Tank;
+
+public class ProjectilesFactory {
+	private ArrayList projectiles;
+	
+	public ProjectilesFactory(){
+		
+	}
+	
+	public void lancerProjectile(Tank lanceur,double amplitude,double hauteur,double longueur){
+		
+	}
+	public void Ajouterprojectiles(){
+		/*
+		 * TODO savoir comment rajoutert des projetiles pendant le déroulement du jeu... Comme dans le collisionneur!
+		 */
+	}
+}
